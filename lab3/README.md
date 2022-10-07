e01ae8c69697f36a34b37144fc26a5cffe5b2002
CianHowell <howelc@rpi.edu>
2022-10-07 10:32:11 -0400

    Lab 3
    Done with lab 3

---
+++ lab3/README.md
@@ -0,0 +1,22 @@
+This lab was rough to be honest. I got a late start on it due to other course work and I wanted to make it look very nice. 
+The part that took me the longest was the homepage, as I wanted to keep the theme of the main RPI website while still having differences
+that were aesthetically pleasing, accessible, and followed HCI principles. Boostrap played a huge part in the creation of the site because
+I did not have enough time to create my own UI especially one that would rival boostrap's offerings. I would have liked to have more time 
+on this because I felt as though on some pages I made minor changes and just copied the information. I would have prefered to add and 
+change information and its formatting to a greater extent. I chose to do a multipage site to keep it closer to what an actual
+ITWS site would likely resemble. My CSS has warnings but they are dynamic warnings due to using var and using browser extenstions so
+please keep that in mind while grading. I did not have to write a lot of JS for this lab because of bootstrap.
+I tried using SMTP js for sending the form. I got it to work then got paywalled so I could not end up using it. I left the JS i used for it commented out. Overall this lab was fun!
+
+
+
+
+Works Cited:
+
+Photos: RPI website, ITWS Facebook, ITWS Blog.
+
+CSS Structure and Media Queries inspired or taken from Cian Howell and Jerry Lu's Qti.ai
+
+Bootstrap Docs used for bootstrap classes https://getbootstrap.com/docs/5.2/getting-started/introduction/
+
+Logos, colors, and fonts from RPI comm site.
---
+++ lab3/index.js
@@ -0,0 +1,50 @@
+// send an email
+function sendEmail() {
+    const formInput = document.getElementById("contact-form").elements;
+    let nameInput = formInput[0].value;
+    let emailInput = formInput[1].value;
+    let phoneInput = formInput[2].value;
+    let messageInput = formInput[3].value;
+
+    let emailBody = "Name: " + nameInput + "%0A" + "Email: " + emailInput + "%0A" + "Phone Number: " + phoneInput + "%0A" + "Message:%0A" + messageInput;
+
+    for (let i = 0; i < emailBody.length; i++) {
+        if (emailBody[i] == " ") {
+            emailBody[i] = "%20";
+        }
+    }
+
+    window.location.href = "mailto:rpi.lab3.burner@outlook.com?subject=ITWS%20Form&body=" + emailBody;
+
+    alert("Form Submitted")
+    // Email.send({
+    //     Host: "in-v3.mailjet.com",
+    //     Username: "6eeecfd65e64bac98e270c1c3d12e9a3",
+    //     Password: "cd1c88b5a110c178de753a0f169b6a08",
+    //     To: 'rpi.lab3.burner@outlook.com',
+    //     From: "rpi.lab3.burner@outlook.com",
+    //     Subject: "ITWS Inqury from " + nameInput,
+    //     Body: emailBody
+    // }).then(
+    //     message => alert(message)
+    // );
+
+}
+
+// Change nav bar based on scroll
+window.onscroll = function () { navChange() };
+function navChange() {
+    var rpiLogo = document.getElementById("ITWS-logo");
+    if(screen.width < 700){
+        rpiLogo.src = "/lab3/Resources/RPI-logo-black.png";
+        rpiLogo.style.height = "3rem";
+        return;
+    }
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        rpiLogo.src = "/lab3/Resources/RPI-logo-black.png";
+        rpiLogo.style.height = "2rem";
+    } else {
+        rpiLogo.src = "/lab3/Resources/RPI-ITWS-logo-black-howelc.svg";
+        rpiLogo.style.height = "5rem";
+    }
+}
